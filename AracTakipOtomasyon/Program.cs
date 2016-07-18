36ce953daac7c78a395bd640e1376c9a9086a7a9
Ramazan Aras <ramazanaras.ra@gmail.com>
2016-07-18 13:45:46 +0300

    masterdann commit

--- AracTakipOtomasyon/Program.cs
+++ AracTakipOtomasyon/Program.cs
@@ -19,6 +19,8 @@ namespace AracTakipOtomasyon
             
             //kodlama 3-navigasyonlu
             //navigasyon geliştirilmesi çekirdeğe eklendi
+
+            //masterdannnnn
         }
     }
 }
